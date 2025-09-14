167126f14d8bd2e0b0317a5f6f5eb0a3f50e86d8
Aravind507 <144810020+Aravind507@users.noreply.github.com>
2025-09-14 16:47:05 +0530

    testing MR conflicts

--- Projects/Java Projects/N_Queens/Main.java
+++ Projects/Java Projects/N_Queens/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         NQueens obj = new NQueens();
         System.out.println("All possible ways");
+        System.out.println("HI there");
         obj.nQueens(4);
     }
 }
