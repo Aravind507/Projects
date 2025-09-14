5f6943512979cec6dc2bc911534dee261be0cfb6
Aravind507 <144810020+Aravind507@users.noreply.github.com>
2025-09-14 17:19:19 +0530

    Added new line in testing3

--- Projects/Java Projects/N_Queens/Main.java
+++ Projects/Java Projects/N_Queens/Main.java
@@ -5,6 +5,7 @@ public class Main {
         NQueens obj = new NQueens();
         System.out.println("All possible ways");
         System.out.println("This is day 2 of learming Git ");
+        System.out.println("this is a new line");
         obj.nQueens(4);
     }
 }
